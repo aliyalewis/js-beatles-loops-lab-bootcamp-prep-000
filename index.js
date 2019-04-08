cfa3e8c26c06f8dd7d517c2682ae015140d0e884
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-08 18:59:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+// i var length =  musicians.length
+  for (let i = 0; i<musicians.length; i++) {
+array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  let i=0;
+  while (i<facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++
+  }
+  return array
+}
+
 function iLoveTheBeatles(num) {
   var array = [];
   do {array.push("I love the Beatles!");
